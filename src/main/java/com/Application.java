9097ce1d6ee2f03570b0a86965bc3ee9890b374a
kavya0805 <kavikarthic8@gmail.com>
2020-05-04 08:32:33 +0530

    jenkins file

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
